26b6346e17fcbfe89f2b6bc064abc1ade481d700
arthur <you@example.com>
2018-11-24 10:58:13 -0300

    Projeto angular gerado

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { ProductComponent } from './list-products/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListProductsComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
