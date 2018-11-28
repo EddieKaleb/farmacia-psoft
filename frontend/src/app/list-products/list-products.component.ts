cd5d0e8c2a2c9c41795d4fe093ba44336b265ba2
arthurferrao <arthurdeferrao@gmail.com>
2018-11-28 12:59:59 -0300

    Listagem de produtos

--- frontend/src/app/list-products/list-products.component.ts
+++ frontend/src/app/list-products/list-products.component.ts
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-products.component.css']
 })
 export class ListProductsComponent implements OnInit {
+  products: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.products = [
+      
+      {
+        name: "Escova de dentes",
+        oldPrice: "5,00",
+        newPrice: "5,00",
+        image: "assets/img/escova.png",
+        quant: "2 unidades",
+        promo: 0
+      },
+      {
+        name: "Pasta de dentes",
+        oldPrice: "3,00",
+        newPrice: "2,50",
+        image: "assets/img/pasta.png",
+        quant: "30ml",
+        promo: 30
+      },
+      {
+        name: "Escova de dentes",
+        oldPrice: "5,00",
+        newPrice: "5,00",
+        image: "assets/img/escova.png",
+        quant: "2 unidades",
+        promo: 0
+      },
+      {
+        name: "Pasta de dentes",
+        oldPrice: "3,00",
+        newPrice: "2,50",
+        image: "assets/img/pasta.png",
+        quant: "30ml",
+        promo: 30
+      },
+      {
+        name: "Escova de dentes",
+        oldPrice: "5,00",
+        newPrice: "5,00",
+        image: "assets/img/escova.png",
+        quant: "2 unidades",
+        promo: 0
+      },
+      {
+        name: "Pasta de dentes",
+        oldPrice: "3,00",
+        newPrice: "2,50",
+        image: "assets/img/pasta.png",
+        quant: "30ml",
+        promo: 30
+      },
+  
+     
+    ]
   }
 
 }
