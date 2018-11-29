611df8045b0f8424d7b0b733958afea36b3bfea8
Eddie Kaleb <kaleb.eddie964@gmail.com>
2018-11-28 23:09:42 -0300

    - Handle para mensagens de erro

---
+++ backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/exceptionhandler/PharmaExceptionHandler.java
@@ -0,0 +1,54 @@
+package br.edu.ufcg.pharma.api.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class PharmaExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		String mensagemUsr = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDev = ex.getCause().toString();
+		return handleExceptionInternal(ex, new Erro(mensagemUsr,mensagemDev), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	
+	/**
+	 * Model para mensagem de erro do usuario e desenvolvedor
+	 * @author eddieklf
+	 */
+	public static class Erro {
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+		
+		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+		
+		
+	}
+	
+}
--- backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/recurso/CategoriaRecurso.java
+++ backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/recurso/CategoriaRecurso.java
@@ -1,11 +1,19 @@
 package br.edu.ufcg.pharma.api.recurso;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.ufcg.pharma.api.model.Categoria;
 import br.edu.ufcg.pharma.api.repositorio.CategoriaRepositorio;
@@ -22,4 +30,18 @@ public class CategoriaRecurso {
 		return categoriaRepositorio.findAll();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+		Categoria categoriaSalva = categoriaRepositorio.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categoriaSalva.getId()).toUri();
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{id}")
+	public Categoria buscarPorId(@PathVariable Integer id) {
+		return categoriaRepositorio.findOne(id);
+	}
 }
