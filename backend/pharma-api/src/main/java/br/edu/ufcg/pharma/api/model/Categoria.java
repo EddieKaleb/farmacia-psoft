01cfd7a6d9a558a0007259d0f485c0cd6b14e6c7
Eddie Kaleb <kaleb.eddie964@gmail.com>
2018-12-08 23:15:21 -0300

    tratamento para recursao nao existente

--- backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/model/Categoria.java
+++ backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/model/Categoria.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
--- backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/recurso/CategoriaRecurso.java
+++ backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/recurso/CategoriaRecurso.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ufcg.pharma.api.evento.RecursoCriadoEvento;
@@ -37,16 +39,21 @@ public class CategoriaRecurso {
 
 	@PostMapping
 	public ResponseEntity<Categoria> criar(@RequestBody @Valid Categoria categoria, HttpServletResponse resposta) {
-
 		Categoria categoriaSalva = categoriaRepositorio.save(categoria);
-		
-		publisher.publishEvent(new RecursoCriadoEvento(this, resposta, categoriaSalva.getId()));
-		
+		publisher.publishEvent(new RecursoCriadoEvento(this, resposta, categoriaSalva.getId()));	
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 
 	@GetMapping("/{id}")
-	public Categoria buscarPorId(@PathVariable Integer id) {
-		return categoriaRepositorio.findOne(id);
+	public ResponseEntity<Categoria> buscarPorId(@PathVariable Integer id) {
+		Categoria categoria = categoriaRepositorio.findOne(id);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Integer id) {
+		this.categoriaRepositorio.delete(id);
+	}
+	
 }
