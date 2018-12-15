1a8104bdead6bb7b08e2eaf33bb83d4a801921c3
arthur <arthurdeferrao@gmail.com>
2018-12-15 14:17:53 -0300

    Correção senha root

--- frontend/src/app/service/products.service.ts
+++ frontend/src/app/service/products.service.ts
@@ -11,58 +11,7 @@ export class ProductsService {
   }
 
   getProducts(): Array<Product>{
-    this.products = [
-      new Product("Escova de dentes", 5.50,5.50,"assets/img/medicamento.png","2 unidades", 0),
-      new Product("Pasta de dentes", 3.00,2.50, "assets/img/medicamento.png","30ml", 30),
-      new Product("Escova de dentes", 5.50,5.50,"assets/img/medicamento.png","2 unidades", 0),
-      new Product("Pasta de dentes", 3.00,2.50, "assets/img/medicamento.png","30ml", 30),
-      new Product("Escova de dentes", 5.50,5.50,"assets/img/medicamento.png","2 unidades", 0),
-      new Product("Pasta de dentes", 3.00,2.50, "assets/img/medicamento.png","30ml", 30),
-      new Product("Escova de dentes", 5.50,5.50,"assets/img/medicamento.png","2 unidades", 0),
-      new Product("Pasta de dentes", 3.00,2.50, "assets/img/medicamento.png","30ml", 30),
-    ];
-    return this.products
-  }
-
-  getProductsCategory(category): Array<Product>{
     
-    let products;
-    if(category === 'medicamentos'){
-      products = [
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/medicamento.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/medicamento.png","30ml", 30),
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/medicamento.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/medicamento.png","30ml", 30),
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/medicamento.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/medicamento.png","30ml", 30),
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/medicamento.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/medicamento.png","30ml", 30),
-      ]
-    }else if(category === 'higiene-pessoal'){
-      products = [
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/higiene.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/higiene.png","30ml", 30),
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/higiene.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/higiene.png","30ml", 30),
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/higiene.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/higiene.png","30ml", 30),
-      ]
-    }else if(category === 'alimentos'){
-      products = [
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/alimento.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/alimento.png","30ml", 30),
-        new Product("Escova de dentes", 5.50,5.50,"assets/img/alimento.png","2 unidades", 0),
-        new Product("Pasta de dentes", 3.00,2.50, "assets/img/alimento.png","30ml", 30),
-      ];
-    }else if(category === 'cosmeticos'){
-      products = [
-
-      ];
-    }else{
-      products = [
-
-      ]
-    }
-    return products;
+    return
   }
 }
