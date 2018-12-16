ce8da7588adef4d574a257fbc80f43dc3202ac15
Gabriel de Sousa <gabriel.sb199@gmail.com>
2018-12-16 17:12:57 -0300

    Alterada exibição de produtos para admin

--- frontend/src/app/admin/admin-products/admin-products.component.ts
+++ frontend/src/app/admin/admin-products/admin-products.component.ts
@@ -4,6 +4,7 @@ import { ProductsService } from 'src/app/service/products.service';
 import { BsModalService } from 'ngx-bootstrap/modal/';
 import { AdminAddProductComponent } from './admin-add-product/admin-add-product.component';
 import { AdminDescontosComponent } from './admin-descontos/admin-descontos.component';
+import Product from 'src/app/model/product.module';
 
 @Component({
   selector: 'app-admin-products',
@@ -17,7 +18,10 @@ export class AdminProductsComponent implements OnInit {
     private route: ActivatedRoute, private modalService: BsModalService) { }
 
   ngOnInit() {
-
+    this.products = [new Product("pasta", 2, 2.5, "/assets/img/alimento.png", 3, 0),
+    new Product("pasta", 2, 2.5, "/assets/img/medicamento.png", 3, 0),
+    new Product("pasta", 2, 2.5, "/assets/img/cosmetico.png", 3, 0),
+    new Product("pasta", 2, 2.5, "/assets/img/higiene.png", 3, 0)];
   }
 
   openAddProduct(){
