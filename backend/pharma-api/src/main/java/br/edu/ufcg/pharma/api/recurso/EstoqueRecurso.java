291083f89009cbbfc57699dcbce7e61fa3daf91c
Eddie Kaleb <kaleb.eddie964@gmail.com>
2018-12-17 11:26:00 -0300

    categorias para estoque

--- backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/recurso/EstoqueRecurso.java
+++ backend/pharma-api/src/main/java/br/edu/ufcg/pharma/api/recurso/EstoqueRecurso.java
@@ -1,5 +1,6 @@
 package br.edu.ufcg.pharma.api.recurso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,27 +20,48 @@ public class EstoqueRecurso {
 
 	@Autowired
 	private EstoqueRepositorio estoqueRepositorio;
-	
+
 	@Autowired
 	private EstoqueServico estoqueServico;
-	
+
 	@GetMapping
 	public List<Estoque> listar() {
 		return estoqueRepositorio.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Estoque> buscarPorId(@PathVariable Integer id) {
 		Estoque estoqueSalvo = this.estoqueRepositorio.findOne(id);
 		return estoqueSalvo != null ? ResponseEntity.ok(estoqueSalvo) : ResponseEntity.notFound().build();
 	}
-	
+
+	@GetMapping("/categoria/{id}")
+	public List<Estoque> buscarPorCategoria(@PathVariable Integer id) {
+		List<Estoque> estoques = this.estoqueRepositorio.findAll();
+		List<Estoque> novoEstoque = new ArrayList<>();
+		if (id >= 1 && id <= 4) {
+			novoEstoque = filtrarPorCategoria(estoques, id);
+		}
+		return novoEstoque;
+	}
+
+	private List<Estoque> filtrarPorCategoria(List<Estoque> estoques, Integer id) {
+		List<Estoque> novoEstoque = new ArrayList<>();
+		for (int i = 0; i < estoques.size(); i++) {
+			if (estoques.get(i).getProduto().getCategoria().getId() == id) {
+				novoEstoque.add(estoques.get(i));
+			}
+		}
+		return novoEstoque;
+
+	}
+
 	@GetMapping("/atualizar/{id}")
 	public ResponseEntity<Estoque> atualizarValidade(@PathVariable Integer id) {
 		Estoque estoqueSalvo = this.estoqueServico.atualizarValidadePorProdutoId(id);
 		return estoqueSalvo != null ? ResponseEntity.ok(estoqueSalvo) : ResponseEntity.notFound().build();
 	}
-	
+
 	@GetMapping("/atualizar")
 	public List<Estoque> atualizarTodasValidades() {
 		List<Estoque> estoques = estoqueRepositorio.findAll();
@@ -48,5 +70,5 @@ public class EstoqueRecurso {
 		}
 		return estoqueRepositorio.findAll();
 	}
-	
+
 }
